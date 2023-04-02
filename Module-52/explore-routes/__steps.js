d020cad54a9d462b15e56920bd9554e40223d633
Tareq35 <a.t.tariqulislam@gmail.com>
2023-04-03 03:02:18 +0600

    code updated

---
+++ Module-52/explore-routes/__steps.js
@@ -0,0 +1,16 @@
+/* 
+1. install react router: npm i react-router-dom.
+2. create a router: use createBrowserRouter.
+3. add RouterProvider and pass router props.
+4. create few routes inside the router.
+
+*/
+
+
+/* 
+things you need to create a route
+1. create a Link: so that you can go to this route
+2. create component: to add what you will show once you to that route.
+3. add route so that react router know the component it needs to display while you are visiting to that route.
+
+*/
---
+++ Module-52/explore-routes/src/App.js
@@ -0,0 +1,48 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './App.css';
+import About from './components/About/About';
+import FriendDetails from './components/FriendDetails/FriendDetails';
+import Friends from './components/Friends/Friends';
+import Home from './components/Home/Home';
+import Products from './components/Products/Products';
+import Main from './layout/Main';
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/', element: <Main></Main>,
+      children: [
+        { path: '/', element: <Home></Home> },
+        { path: '/home', element: <Home></Home> },
+        { path: '/products', element: <Products></Products> },
+
+        {
+          path: '/friends',
+          loader: async () => {
+            return fetch('https://jsonplaceholder.typicode.com/users')
+          },
+          element: <Friends></Friends>
+        },
+        {
+          path: '/friend/:friendId',
+          loader: async ({ params }) => {
+            // console.log(params.friendId);
+            return fetch(`https://jsonplaceholder.typicode.com/users/${params.friendId}`)
+          },
+          element: <FriendDetails></FriendDetails>
+        }
+      ]
+    },
+
+    { path: '/about', element: <About></About> },
+    { path: '/*', element: <div>This route not found!</div> },
+
+  ])
+  return (
+    <div className="App">
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
